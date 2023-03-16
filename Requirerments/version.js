920c5454069aaabf2cf7cc2038929bbe78ae3175
Gamearoo Codes <gamearoo10@gmail.com>
2023-03-16 16:15:19 -0400

    Create version.js

---
+++ Requirerments/version.js
@@ -0,0 +1,4 @@
+exports.latest = "v1";
+exports.outdated = ["v0"]; //v0 is used as a placeholder so the function will work till a future version where versions aren't coded in anymore
+
+exports.favcon = "https://gamearoo.top/ram-api-img/favicon.png";
