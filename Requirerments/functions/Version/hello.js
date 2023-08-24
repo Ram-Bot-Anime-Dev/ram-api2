c214525214ffa1b373ccbf8df5280965c7f4d965
gamearoo_codes <50187502+GamearooCodes@users.noreply.github.com>
2023-08-24 18:18:29 -0400

    changes

--- Requirerments/functions/Version/hello.js
+++ Requirerments/functions/Version/hello.js
@@ -1,16 +1,11 @@
 exports.hello_version_one = {
-    textupdateArray: [
-        `Hello`,
-        `Hi`,
-        "Hi, How are you?",
-        "Hello, How are you?",
-    ],
-    imageupdateArray: [
-        "http://rambot.xyz/ram-api2-images/hello/hello1.gif",
-        "http://rambot.xyz/ram-api2-images/hello/hello2.gif",
-        "http://rambot.xyz/ram-api2-images/hello/hello3.gif",
-        "http://rambot.xyz/ram-api2-images/hello/hello4.gif",
-        "https://rambot.xyz/ram-api2-images/hello/hello5.gif"
-
-    ]
-}
+  textupdateArray: [`Hello`, `Hi`, "Hi, How are you?", "Hello, How are you?"],
+  imageupdateArray: [
+    "http://rambot.xyz/ram-api2-images/hello/hello1.gif",
+    "http://rambot.xyz/ram-api2-images/hello/hello1.gif",
+    "http://rambot.xyz/ram-api2-images/hello/hello2.gif",
+    "http://rambot.xyz/ram-api2-images/hello/hello3.gif",
+    "http://rambot.xyz/ram-api2-images/hello/hello4.gif",
+    "https://rambot.xyz/ram-api2-images/hello/hello5.gif",
+  ],
+};
--- Requirerments/functions/Version/nekopara.js
+++ Requirerments/functions/Version/nekopara.js
@@ -1,6 +1,7 @@
 exports.nekopara_version_one = {
   imageupdateArray: [
     "https://rambot.xyz/ram-api2-images/NekoPara/nekopara1.jpg",
+    "https://rambot.xyz/ram-api2-images/NekoPara/nekopara1.jpg",
     "https://rambot.xyz/ram-api2-images/NekoPara/nekopara2.gif",
     "https://rambot.xyz/ram-api2-images/NekoPara/nekopara3.gif",
     "https://rambot.xyz/ram-api2-images/NekoPara/nekopara4.gif",
